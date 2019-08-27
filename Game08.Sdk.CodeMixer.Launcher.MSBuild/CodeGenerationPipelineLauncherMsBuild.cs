960b1d5aee23925964f56f768f1712b6fadc31f8
Yevgen <55064156+YevgenNabokov@users.noreply.github.com>
2019-08-28 00:15:20 +0200

    Launcher for MsBuild workspace.

---
+++ Game08.Sdk.CodeMixer.Launcher.MSBuild/CodeGenerationPipelineLauncherMsBuild.cs
@@ -0,0 +1,30 @@
+﻿using Game08.Sdk.CodeMixer.Environment;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Text;
+
+namespace Game08.Sdk.CodeMixer.Launcher.MSBuild
+{
+    public class CodeGenerationPipelineLauncherMsBuild
+    {
+        private readonly IFileSystem fileSystem;
+
+        public CodeGenerationPipelineLauncherMsBuild(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public void Execute(string solutionPath, string pipelineConfigurationPath)
+        {
+            var vsi = MSBuildLocator.RegisterDefaults();
+            var workspace = MSBuildWorkspace.Create();            
+            var solution = workspace.OpenSolutionAsync(solutionPath).Result;
+
+            var launcher = new CodeGenerationPipelineLauncher(workspace, this.fileSystem);
+            launcher.Launch(pipelineConfigurationPath);
+        }
+    }
+}
---
+++ Game08.Sdk.CodeMixer.Launcher.MSBuild/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace Game08.Sdk.CodeMixer.Launcher.MSBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeGenerationPipelineLauncherMsBuild launcher = new CodeGenerationPipelineLauncherMsBuild(new FileSystem());
+            launcher.Execute(args[0], args[1]);
+        }
+    }
+}
