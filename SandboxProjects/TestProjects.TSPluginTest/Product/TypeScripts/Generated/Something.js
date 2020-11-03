c888a0dcda109ad1b34f7cdad9e269e3657e5851
Yevgen Nabokov <55064156+YevgenNabokov@users.noreply.github.com>
2020-11-03 22:24:08 +0100

    Package update.

---
+++ SandboxProjects/TestProjects.TSPluginTest/Product/TypeScripts/Generated/Something.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Something = /** @class */ (function () {
+    function Something(a) {
+        this.a = a;
+    }
+    return Something;
+}());
+exports.Something = Something;
+//# sourceMappingURL=Something.js.map
