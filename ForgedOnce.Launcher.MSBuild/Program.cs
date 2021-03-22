3a3f9bc7e51b1586cd366fb918ce80015526a2b8
Yevgen Nabokov <55064156+YevgenNabokov@users.noreply.github.com>
2021-03-23 00:38:34 +0100

    Added better command line arguments.

--- ForgedOnce.Launcher.MSBuild/Program.cs
+++ ForgedOnce.Launcher.MSBuild/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ForgedOnce.Core.Logging;
 using ForgedOnce.Environment;
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.IO.Abstractions;
 
 namespace ForgedOnce.Launcher.MSBuild
@@ -9,13 +11,40 @@ namespace ForgedOnce.Launcher.MSBuild
     {
         static void Main(string[] args)
         {
+            RootCommand rootCommand = new RootCommand(description: "Runs code generation.")
+            {
+                new Option<string>(
+                    aliases: new [] { "--solution", "-s" },
+                    description: "The path to the target solution."),
+                new Option<string>(
+                    aliases: new [] { "--config", "-c" },
+                    description: "The path to the code generation pipeline config."),
+                new Option<bool>(
+                    aliases: new [] { "--log", "-l" },
+                    description: "Allows to record log when no error occurred.",
+                    getDefaultValue: () => false)
+            };
+
+            rootCommand.Handler = CommandHandler.Create<string, string, bool>(Execute);
+            rootCommand.Invoke(args);
+        }
+
+        static void Execute(string solution, string config, bool log = false)
+        {
             var collectionLogger = new CollectionLogger();
             var fileSystem = new FileSystem();
 
             try
             {
                 CodeGenerationPipelineLauncherMsBuild launcher = new CodeGenerationPipelineLauncherMsBuild(fileSystem, collectionLogger);
-                launcher.Execute(args[0], args[1]);
+                launcher.Execute(solution, config);
+
+                if (log)
+                {
+                    var textLogger = new TextLogger(fileSystem);
+                    textLogger.WriteMany(collectionLogger.Records);
+                    Console.WriteLine($"Log file: {textLogger.InitializedFilePath}");
+                }
             }
             catch (Exception ex)
             {
